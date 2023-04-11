116ed7824f8cb08c39b38b567d4a9824fc80220d
pater <agustinpaternoster@gmail.com>
2023-04-11 19:30:06 +0200

    fin punish funtion

--- test.js
+++ test.js
@@ -27,10 +27,85 @@ playGame(){return};
       }
 
     }
-*/
+
 
 for ( var i = 0 ,) {
         console.log( `La Partida ha comenzado, Puntuacion: ${ clase_partida.js }` );
         let playerSelect = Math.floor( Math.random() * this.playerNum );
         console.log( this.players[ playerSelect ].name );
+*/
+        playGame () {
+          console.log( `La Partida ha comenzado` );
+          console.log( this.scoreBoard.players );
+          for ( let i = 0; i < 10; i++ ) {
+            let playerSelect = Math.floor( Math.random() * this.playerNum );
+            let playerSelectName = this.players[ playerSelect ].name;
+            let playerSelectscore = this.scoreBoard.players[ playerSelectName ] + this.rollDice();
+            this.scoreBoard.players[ playerSelectName ] = playerSelectscore;
+            console.log( this.scoreBoard.players );
+            for ( const [ key, value ] of Object.entries( this.scoreBoard.players ) ) {if(value >=10){
+              return console.log(`el jugador ${key} ha ganado la partida`)
+            }
+              ;
+            }
+          }
+        }
 
+        playGame(){
+          console.log( `La Partida ha comenzado` );
+          console.log( this.scoreBoard.players );
+          for ( const [ key, value ] of Object.entries( this.scoreBoard.players ) ) {
+            if(value >=10){ 
+               return console.log(`el jugador ${key} ha ganado la partida`)
+            }
+            let playerSelect = Math.floor( Math.random() * this.playerNum );
+            let playerSelectName = this.players[ playerSelect ].name;
+            let playerSelectscore = this.scoreBoard.players[ playerSelectName ] + this.rollDice();
+            this.scoreBoard.players[ playerSelectName ] = playerSelectscore;
+            console.log( this.scoreBoard.players );
+        }}
+
+
+        playGame () {
+          console.log( `La Partida ha comenzado` );
+          console.log( this.scoreBoard.players );
+          for ( let i = 0; i < 20; i++ ) {
+            let playerSelect = Math.floor( Math.random() * this.playerNum );
+            let playerSelectName = this.players[ playerSelect ].name;
+            let playerSelectscore = this.scoreBoard.players[ playerSelectName ] + this.rollDice();
+            this.scoreBoard.players[ playerSelectName ] = playerSelectscore;
+            console.log( this.scoreBoard.players );
+            for ( const [ key, value ] of Object.entries( this.scoreBoard.players ) ) {if(value >=20){
+              return console.log(`El jugador ${key} ha ganado`);
+            }
+              ;
+            }
+          }
+        }
+        punishment(players){
+          let unluckyNumber = this.rollDice()
+          if (this.rollDice() === 0){
+            /* let playerSelect = Math.floor(Math.random() * players);
+            let playerSelectName = this.players[playerSelect].name;
+            let playerSelectscore = 0  */
+            console.log("unluki")
+          }
+        }
+
+        /* playGame() {
+          console.log(`La Partida ha comenzado`);
+          console.log(this.scoreBoard.players);
+          const scorePlayers = this.scoreBoard.players
+          for (const [key, value] of scorePlayers) {
+            if (value >= 20) {
+              return console.log(`El jugador ${key} ha ganado`);
+            }
+            
+            let playerSelect = Math.floor(Math.random() * this.playerNum);
+            let playerSelectName = this.players[playerSelect].name;
+            let playerSelectscore =
+              scorePlayers[playerSelectName] + this.rollDice();
+            scorePlayers[playerSelectName] = playerSelectscore;
+            console.log(this.scoreBoard.players);
+          }
+        } */
